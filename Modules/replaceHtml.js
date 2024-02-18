480809af2af2f4723eda839f72730195befbcf8d
sohamkl <soham.kulkarni138@gmail.com>
2024-02-18 14:50:14 +1300

    updated project.js

---
+++ Modules/replaceHtml.js
@@ -0,0 +1,17 @@
+module.exports = function replaceHtml(template, product){
+    //let output = productListHtml.replace('{{%IMAGE%}}', prod.productImage);
+    let output = template;
+    output = output.replace('{{%IMAGE%}}', product.productImage);
+    output = output.replace('{{%NAME%}}', product.name);
+    output = output.replace('{{%MODELNAME%}}', product.modelName);
+    output = output.replace('{{%MODELNO%}}', product.modelNumber);
+    output = output.replace('{{%SIZE%}}', product.size);
+    output = output.replace('{{%CAMERA%}}', product.camera);
+    output = output.replace('{{%PRICE%}}', product.price);
+    output = output.replace('{{%COLOR%}}', product.color);
+    output = output.replace('{{%ID%}}', product.id);
+    output = output.replace('{{%ROM%}}', product.ROM);
+    output = output.replace('{{%DESC%}}', product.Description);
+
+    return output;    
+}
--- project.js
+++ project.js
@@ -2,10 +2,12 @@ const express = require('express');
 const fs = require('fs');
 
 let app = express();
-let products = JSON.parse(fs.readFileSync('./Data/Phones.json', 'utf-8'))
+let products = JSON.parse(fs.readFileSync('iPhones.json', 'utf-8'))
 
-//GET - api/iPhones
-app.get('/api/v1/Phones', (req, res) => {
+app.use(express.json())
+
+//GET - api/Phones
+const getAllProducts = (req, res) => {
     res.status(200).json({
         status: "success",
         count: products.length,
@@ -13,7 +15,108 @@ app.get('/api/v1/Phones', (req, res) => {
             products: products
         }
     })
-})
+}
+//GET - api/v1/Phones/id
+const getProduct = (req, res) => {
+    //console.log(req.params);
+    const id = +req.params.id*1;
+
+    let product = products.find(el => el.id === id)
+
+    if(!product){
+        return res.status(404).json({
+            status: "fail",
+            message: 'Phone with ID ' +id+ ' is not found'
+        })
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            product: product
+        }
+    });
+}
+//POST - /api/v1/Phones
+const createProduct = (req, res) => {
+    const newId = products[products.length - 1].id + 1;
+
+    const newProduct = Object.assign({id: newId}, req.body)
+
+    products.push(newProduct);
+
+    fs.writeFile('iPhones.json', JSON.stringify(products), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                products: newProduct
+            }
+        })
+    })
+    //res.send('Created');
+}
+//PATCH = api.v1/Phones/id
+const updateProduct = (req, res) => {
+    let id = req.params.id * 1;
+    let productToUpdate = products.find(el => el.id === id);
+
+    if(!productToUpdate){
+        res.status(404).json({
+            status: "fail",
+            message: 'No product object with ID ' +id+ ' is found'
+        })
+    }
+    let index = products.indexOf(productToUpdate);
+
+    Object.assign(productToUpdate, req.body);
+    products[index] = productToUpdate;
+
+    fs.writeFile('iPhones.json', JSON.stringify(products), (err) => {
+        res.status(200).json({
+            status: "success",
+            data: {
+                product: productToUpdate
+            }
+        })
+    })
+}
+//DELETE = api.v1/Phones/id
+const deleteProduct = (req, res) => {
+    let id = req.params.id*1;
+    let productToDelete = products.find(el => el.id === id);
+
+    if(!productToDelete){
+        res.status(404).json({
+            status: "fail",
+            message: 'No product object with ID ' +id+ ' is found'
+        })
+    }
+
+    let index = products.indexOf(productToDelete);
+
+    products.splice(index, 1);
+
+    fs.writeFile('iPhones.json', JSON.stringify(products), (err) => {
+        res.status(204).json({
+            status: "success",
+            data: {
+                product: null
+            }
+        })
+    })
+
+}
+
+app.route('/api/v1/Phones')
+    .get(getAllProducts)
+    .post(createProduct)
+
+app.route('/api/v1/Phones/:id')
+    .get(getProduct)
+    .patch(updateProduct)
+    .delete(deleteProduct)
+
+
 //Create a server
 const port = 3000;
 app.listen(port, () => {
